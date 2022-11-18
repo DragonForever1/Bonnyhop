908199b0b7bf6b3d8913481441e4747144180f34
DragonForever1 <104247202+DragonForever1@users.noreply.github.com>
2022-11-18 23:58:50 +0300

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,7 @@
+#include <cstdint>
+#include <cstddef>
+namespace offsets {
+	constexpr ::std::ptrdiff_t m_fFlags = 0x104;
+	constexpr ::std::ptrdiff_t dwForceJump = 0x52B7BCC;
+	constexpr ::std::ptrdiff_t dwLocalPlayer = 0xDE6964;
+}
