4fa0f1198ba90bfe7a31ccfc72af50efecd963fd
DragonForever1 <104247202+DragonForever1@users.noreply.github.com>
2022-11-19 00:06:13 +0300

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -5,3 +5,6 @@ namespace offsets {
 	constexpr ::std::ptrdiff_t dwForceJump = 0x52B7BCC;
 	constexpr ::std::ptrdiff_t dwLocalPlayer = 0xDE6964;
 }
+// Offsets has regular renew. 
+// See this guy, if you won't to use don't work programm: hazedumper
+//Just click on csgo.hpp and copy all offsets
